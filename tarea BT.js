451ed70e8499fefbf8d9e9e79a47f6ab7232fac9
Andrea García <89950395+andreagg19@users.noreply.github.com>
2022-11-28 23:58:25 -0600

    Update tarea BT.js

--- tarea BT.js
+++ tarea BT.js
@@ -68,25 +68,67 @@ class BinaryTree{
         }
     }
 
-}
+    postOrder(){
+        let result=[];
+        let ope=[];
+        while(ope.length > 0){
+            if(!isNaN(ope[ope.length-1])){
+                result.push(ope.pop());
+            }else{
+                let aux = ope.pop();
+                result.push(this.operator(aux, result.pop(), result.pop()))
+            }
+        }
+        return result[0];
+            
+    }        
+
+    preOrder(){
+        
+    }
+
+    }
+
 
 let tree=new BinaryTree();
-let node=new Node(4);
+let node=new Node(3);
 tree.add(node);
-node=new Node('+');
+node=new Node('*');
 tree.add(node);
-node=new Node(5);
+node=new Node(9);
 tree.add(node);
 node=new Node('-');
 tree.add(node);
 node=new Node(6);
 tree.add(node);
+node=new Node('*');
+tree.add(node);
+node=new Node(3);
+tree.add(node);
 node=new Node('/');
 tree.add(node);
 node=new Node(2);
 tree.add(node);
+node=new Node('+');
+tree.add(node);
+node=new Node(3);
+tree.add(node);
 node=new Node('*');
 tree.add(node);
-node=new Node(7);
+node=new Node(6);
+tree.add(node);
+node=new Node('+');
+tree.add(node);
+node=new Node(5);
+tree.add(node);
+node=new Node('*');
+tree.add(node);
+node=new Node(4);
+tree.add(node);
+node=new Node('/');
+tree.add(node);
+node=new Node(2);
 tree.add(node);
 console.log(tree.inorder());
+console.log(tree.preOrder('+','-','*',3,9,/,'*',6,3,2,'+','*',3,6,/,'*',5,4,2));
+console.log(tree.postOrder(3,9,'*',6,3,'*',2,'/','-',3,6,'*',5,4,2,'/','+','+'));
